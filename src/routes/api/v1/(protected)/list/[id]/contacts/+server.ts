45d16089d00302fd8a253a8d5436f8a40d4b4be4
Raqueebuddin Aziz <raqueebuddin.aziz.314@gmail.com>
2024-04-14 09:54:33 +0530

    fix list api endpoint

--- src/routes/api/v1/(protected)/list/[id]/contacts/+server.ts
+++ src/routes/api/v1/(protected)/list/[id]/contacts/+server.ts
@@ -35,24 +35,28 @@ export const GET = (async (event) => {
 
 	const parsedMetadata = listMetadataSchema.parse(node.metadata);
 
-	const rows = await db
-		.select({ id: nodes.id, contacts: sql<string>`metadata->'contacts'` })
-		.from(nodes)
-		.where(
-			and(
-				inArray(
-					nodes.id,
-					parsedMetadata.contacts.map((contact) => contact.nodeId)
-				),
-				eq(nodes.userId, event.locals.user.id)
-			)
-		);
-	const contacts = rows.flatMap((row) => {
-		const contacts = JSON.parse(row.contacts) as TContact[];
-		return contacts
-			.map((c) => ({ ...c, nodeId: row.id }))
-			.filter((c) => parsedMetadata.contacts.some((contact) => contact.phones.includes(c.phone)));
-	});
+	let contacts: (TContact & { nodeId: TNode['id'] })[] = [];
+	const contactIds = parsedMetadata.contacts.map((contact) => contact.nodeId);
+	if (contactIds.length > 0) {
+		const rows = await db
+			.select({ id: nodes.id, contacts: sql<string>`metadata->'contacts'` })
+			.from(nodes)
+			.where(
+				and(
+					inArray(
+						nodes.id,
+						parsedMetadata.contacts.map((contact) => contact.nodeId)
+					),
+					eq(nodes.userId, event.locals.user.id)
+				)
+			);
+		contacts = rows.flatMap((row) => {
+			const contacts = JSON.parse(row.contacts) as TContact[];
+			return contacts
+				.map((c) => ({ ...c, nodeId: row.id }))
+				.filter((c) => parsedMetadata.contacts.some((contact) => contact.phones.includes(c.phone)));
+		});
+	}
 
 	return json({
 		success: true,
