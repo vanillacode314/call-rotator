4a23e407d552f978c9eba0751f9a67d31b2520d0
Raqueebuddin Aziz <raqueebuddin.aziz.314@gmail.com>
2024-04-22 08:47:39 +0530

    optimize by-path nodes query

--- src/routes/api/v1/(protected)/nodes/by-path/+server.ts
+++ src/routes/api/v1/(protected)/nodes/by-path/+server.ts
@@ -1,6 +1,7 @@
+import { GET_NODES_BY_PATH_QUERY } from '$/consts/sql';
 import { nodeSchema, nodes } from '$/lib/db/schema.libsql';
 import { json } from '@sveltejs/kit';
-import { and, eq, sql } from 'drizzle-orm';
+import { and, eq, isNull, or, sql } from 'drizzle-orm';
 import { z } from 'zod';
 import type { RequestHandler } from './$types';
 import { getInputSchema } from './schema';
@@ -18,9 +19,7 @@ export const GET = (async (event) => {
 		);
 	}
 
-	let path = result.data.path;
-	path = path.trim();
-	if (!path.startsWith('/'))
+	if (!result.data.path.startsWith('/'))
 		return json(
 			{
 				success: false,
@@ -29,82 +28,58 @@ export const GET = (async (event) => {
 			},
 			{ status: 400 }
 		);
-
-	const parts = path.substring(1).split('/');
+	const path = result.data.path.trim();
 	const db = event.locals.db;
-	let node: TNode;
+	let rows: TNode[];
 	if (path === '/') {
-		[node] = await db
-			.select()
-			.from(nodes)
-			.where(and(eq(nodes.id, 0), eq(nodes.userId, event.locals.user.id)))
-			.limit(1);
+		if (result.data.includeChildren) {
+			rows = await db
+				.select()
+				.from(nodes)
+				.where(
+					and(
+						or(isNull(nodes.parent_id), eq(nodes.parent_id, 0)),
+						eq(nodes.userId, event.locals.user.id)
+					)
+				);
+		} else {
+			rows = await db
+				.select()
+				.from(nodes)
+				.where(and(isNull(nodes.parent_id), eq(nodes.userId, event.locals.user.id)));
+		}
 	} else {
-		const {
-			rows: [row]
-		} = await db.run(sql`with recursive
-									fs(n, path, id, next_part) as (
-											select 1, ${path}, 0, ${parts[0]}
-											union
-											select
-													n + 1,
-													substr(substr(path, 2), instr(substr(path, 2), '/')),
-													nodes.id,
-													iif(
-															instr(substr(path, instr(path, next_part) + length(next_part) + 1), '/')
-															- 1
-															= -1,
-															substr(substr(path, instr(path, next_part) + length(next_part) + 1), 1),
-															substr(
-																	substr(path, instr(path, next_part) + length(next_part) + 1),
-																	1,
-																	instr(
-																			substr(path, instr(path, next_part) + length(next_part) + 1),
-																			'/'
-																	)
-																	- 1
-															)
-													)
-											from fs, nodes
-											where
-													instr(path, '/') != 0 and nodes.parent_id = fs.id and next_part = nodes.name
-									)
-							select *
-							from nodes
-							where id = (select id from fs order by n desc limit 1) and nodes.name = ${parts.at(-1)} and userId = ${event.locals.user.id}`);
-
-		if (row === undefined)
-			return json(
-				{ success: false, status: 404, errors: [{ message: 'Node not found', code: 'NOT_FOUND' }] },
-				{ status: 404 }
-			);
-
-		node = nodeSchema
-			.extend({
-				metadata: z
-					.string()
-					.or(z.record(z.string(), z.string()))
-					.nullable()
-					.transform((value) => {
-						return (
-							value === null ? null : typeof value === 'string' ? JSON.parse(value) : value
-						) as TNode['metadata'];
-					})
-			})
-			.parse(row);
+		rows = await db.all(
+			sql.raw(GET_NODES_BY_PATH_QUERY(path, event.locals.user.id, result.data.includeChildren))
+		);
 	}
 
-	let children: TNode[] = [];
-	if (result.data.includeChildren) {
-		children = await db
-			.select()
-			.from(nodes)
-			.where(and(eq(nodes.parent_id, node.id), eq(nodes.userId, event.locals.user.id)));
-	}
+	if (rows === undefined || rows.length === 0)
+		return json(
+			{ success: false, status: 404, errors: [{ message: 'Node not found', code: 'NOT_FOUND' }] },
+			{ status: 404 }
+		);
+
+	rows = nodeSchema
+		.extend({
+			metadata: z
+				.string()
+				.or(z.record(z.string(), z.string()))
+				.nullable()
+				.transform((value) => {
+					return (
+						value === null ? null : typeof value === 'string' ? JSON.parse(value) : value
+					) as TNode['metadata'];
+				})
+		})
+		.array()
+		.parse(rows);
+
+	const node = rows.shift();
 
 	return json({
 		success: true,
 		status: 200,
-		data: { node, children }
+		data: { node, children: rows }
 	});
 }) satisfies RequestHandler;
