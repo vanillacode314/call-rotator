ce35cb3e63255b56b0e1419f83c9440d73224b12
Raqueebuddin Aziz <raqueebuddin.aziz.314@gmail.com>
2024-07-31 03:06:13 +0530

    add resetdb option on error page

--- apps/client/src/routes/+error.svelte
+++ apps/client/src/routes/+error.svelte
@@ -3,6 +3,7 @@
 	import Button from '$/components/ui/button/button.svelte';
 	import Nav from '$/components/Nav.svelte';
 	import { page } from '$app/stores';
+	import { resetDb } from '$/lib/db/sqlocal.db';
 </script>
 
 <div class="content-grid h-full grid-rows-[auto_1fr]">
@@ -16,6 +17,16 @@
 				<Button href="/" size="lg" class="flex h-14 gap-2 text-xl">
 					<span class="i-carbon:home"></span>
 					Home
+				</Button>
+				<Button
+					on:click={() => {
+						resetDb('database.sqlite3');
+					}}
+					size="lg"
+					class="flex h-14 gap-2 text-xl"
+				>
+					<span class="i-carbon:reset"></span>
+					Reset Local Database
 				</Button>
 			</h1>
 		</div>
