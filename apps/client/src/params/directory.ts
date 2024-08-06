12e66639a508434d21a5de82ef959d3635319072
Raqueebuddin Aziz <raqueebuddin.aziz.314@gmail.com>
2024-08-06 23:02:39 +0530

    fix api

--- apps/client/src/params/directory.ts
+++ apps/client/src/params/directory.ts
@@ -1,4 +1,5 @@
 import type { ParamMatcher } from '@sveltejs/kit';
 
-const match: ParamMatcher = (param) => param === '' || !/^[\w\s/-]+\.[\w\s]+$/.test(param);
+const match: ParamMatcher = (param) =>
+	(param === '' || !/^[\w\s/-]+\.[\w\s]+$/.test(param)) && !param.startsWith('api/');
 export { match };
