88618340a0f706c11fe563dcc505bfd9f61bea14
Raqueebuddin Aziz <raqueebuddin.aziz.314@gmail.com>
2024-08-06 22:17:32 +0530

    setup for spa/capacitor

--- apps/client/src/routes/+error.svelte
+++ apps/client/src/routes/+error.svelte
@@ -4,6 +4,7 @@
 	import Nav from '$/components/Nav.svelte';
 	import { page } from '$app/stores';
 	import { resetDb } from '$/lib/db/sqlocal.db';
+	import { isOnline } from '$/stores/online';
 </script>
 
 <div class="content-grid h-full grid-rows-[auto_1fr]">
@@ -11,13 +12,17 @@
 	<main class="full-width">
 		<div class="grid place-content-center text-center">
 			<h1 class="flex flex-col gap-4">
-				<span class="text-lg">
-					{$page.status} : {$page.error?.message ?? 'Unknown Error'}
-				</span>
-				<Button href="/" size="lg" class="flex h-14 gap-2 text-xl">
-					<span class="i-carbon:home"></span>
-					Home
-				</Button>
+				{#if $isOnline}
+					<span class="text-lg">
+						{$page.status} : {$page.error?.message ?? 'Unknown Error'}
+					</span>
+					<Button href="/" size="lg" class="flex h-14 gap-2 text-xl">
+						<span class="i-carbon:home"></span>
+						Home
+					</Button>
+				{:else}
+					<span class="text-lg"> You are offline </span>
+				{/if}
 			</h1>
 		</div>
 	</main>
