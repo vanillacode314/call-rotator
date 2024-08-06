4b78144a1f275de09f0f6b59e598728fcaf5ffbc
Raqueebuddin Aziz <raqueebuddin.aziz.314@gmail.com>
2024-08-07 00:08:17 +0530

    update cors

--- apps/server/server/middleware/1-cors.ts
+++ apps/server/server/middleware/1-cors.ts
@@ -1,6 +1,7 @@
 export default eventHandler((event) => {
+	const origin = getHeader(event, 'origin');
 	handleCors(event, {
-		origin: '*',
+		origin: origin ? [origin] : 'null',
 		methods: ['HEAD', 'GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
 		credentials: true,
 		preflight: { statusCode: 204 }
